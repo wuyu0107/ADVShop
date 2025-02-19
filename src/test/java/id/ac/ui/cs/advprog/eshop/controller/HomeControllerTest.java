56985dbec1fe357a1a15125f789cdd0d113f07de
wuyu0107 <mkim050107@gmail.com>
2025-02-19 10:55:32 +0700

    Make code coverage to 100

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -1,4 +1,13 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HomeControllerTest {
+    @Test
+    void homeReturnsHomepage() {
+        HomeController controller = new HomeController();
+        String home = controller.home();
+        assertEquals("Homepage", home);
+    }
 }
