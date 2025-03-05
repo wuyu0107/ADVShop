8b49e74dab9c8d6f63fbabc51780736dd918de57
wuyu0107 <mkim050107@gmail.com>
2025-03-05 21:07:49 +0700

    [RED] Add tests for OrderServiceImpl

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -0,0 +1,139 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceImplTest {
+
+    @InjectMocks
+    OrderServiceImplTest orderService;
+
+    @Mock
+    OrderRepository orderRepository;
+    List<Order> orders;
+
+    @BeforeEach
+    void setup() {
+        List<Product> products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductID("eb558e9f-1c39-460e-8860-71af6af63bdg");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+        products.add(product1);
+
+        orders = new ArrayList<>();
+        Order order1 =  new Order("13652556-012a-4c07-b546-54eb1396d79b", products,
+                1708560000L, "Safira Sudrajat");
+        orders.add(order1);
+        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af3856b078", products,
+                1708570000L, "Safira Sudrajat");
+        orders.add(order2);
+    }
+
+    // Happy path test
+    @Test
+    void testCreateOrder() {
+        Order order = orders.get(1);
+        doReturn(order).when(orderRepository).save(order);
+
+        Order result = orderService.createOrder(order);
+        verify(orderRepository, times(1)).save(order);
+        assertEquals(order.getId(), result.getId());
+    }
+
+    @Test
+    void testUpdateStatus() {
+        Order order = orders.get(1);
+        Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(),
+                order.getAuthor(), OrderStatus.SUCCESS.getStatus());
+        doReturn(order).when(orderRepository).findById(order.getId());
+        doReturn(newOrder).when(orderRepository).save(any(Order.class));
+
+        Order result = orderService.updateStatus(order.getId(), OrderStatus.SUCCESS.getStatus());
+
+        assertEquals(order.getId(), result.getId());
+        assertEquals(OrderStatus.SUCCESS.getStatus(), result.getStatus());
+        verify(orderRepository, times(1)).save(any(Order.class));
+    }
+
+    @Test
+    void testFindByIdIfIdFound() {
+        Order order = orders.get(1);
+        doReturn(order).when(orderRepository).findById(order.getId());
+
+        Order result = orderService.findById(order.getId());
+        assertEquals(order.getId(), result.getId());
+    }
+
+    @Test
+    void testFindAllByAuthorIfAuthorCorrect() {
+        Order order = orders.get(1);
+        doReturn(orders).when(orderRepository).findAllByAuthor(order.getAuthor());
+
+    }
+
+    // Unhappy path test
+    @Test
+    void testCreateOrderIfAlreadyExists() {
+        Order order = orders.get(1);
+        doReturn(order).when(orderRepository).findById(order.getId());
+
+        assertNull(orderService.createOrder(order));
+        verify(orderRepository, times(0)).save(order);
+    }
+
+    @Test
+    void testUpdateStatusInvalidStatus() {
+        Order order = orders.get(1);
+        doReturn(order).when(orderRepository).findById(order.getId());
+
+        assertThrows(IllegalArgumentException.class,
+                () -> orderService.updateStatus(order.getId(), "MEOW"));
+        verify(orderRepository, times(0)).save(any(Order.class));
+    }
+
+    @Test
+    void testFindByIdIfIdNotFound() {
+        doReturn(null).when(orderRepository).findById("zczc");
+        assertNull(orderService.findById("zczc"));
+    }
+
+    // Happy path test
+    @Test void testFindAllByAuthorIfAuthorCorrect() {
+        Order order = orders.get(1);
+        doReturn(order).when(orderRepository).findAllByAuthor(order.getAuthor());
+
+        List<Order> results = orderService.findAllByAUthor(order.getAuthor());
+        for (Order result : results) {
+            assertEquals(order.getId(), result.getId());
+        }
+        assertEquals(2, results.size());
+    }
+
+    // Unhappy path test
+    @Test
+    void testFindAllByAuthorIfAllLowercase() {
+        Order order = orders.get(1);
+        doReturn(new ArrayList<Order>()).when(orderRepository).findAllByAuthor(order.getAuthor().toLowerCase());
+
+        List<Order> results = orderService.findAllByAuthor(
+                order.getAuthor().toLowerCase());
+        assertTrue(results.isEmpty());
+    }
+
+}
