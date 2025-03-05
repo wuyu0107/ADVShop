155371d392ae268ecb9ab72531f2ede32b76e76a
wuyu0107 <mkim050107@gmail.com>
2025-03-05 19:44:04 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -62,7 +62,7 @@ class OrderTest {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products,
                 1708560000L, "Safira Sudrajat", "SUCCESS");
 
-        assertEquals("SUCESS", order.getStatus());
+        assertEquals("SUCCESS", order.getStatus());
     }
 
     // Unhappy path test
