dd9cdfffa3eb5c22a78cdcd7efa9414aa5304fa6
wuyu0107 <mkim050107@gmail.com>
2025-03-05 19:34:29 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -62,7 +62,7 @@ class OrderTest {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", this.products,
                 1708560000L, "Safira Sudrajat", "SUCCESS");
 
-        assertsEquals("SUCESS", order.getStats());
+        assertEquals("SUCESS", order.getStatus());
     }
 
     // Unhappy path test
